9b34ac0e217eb340599543c3cb49548523c153fe
Shivam Gravity <shivamgravity@gmail.com>
2026-03-24 00:28:53 +0530

    updated shell command

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Generate historical data if not present, then start server
 python generate_history.py
+python main.py --demo
 uvicorn api.server:app --host 0.0.0.0 --port ${PORT:-8000}
