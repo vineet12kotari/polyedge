83231c57a6c2e9294ecada2aebe59d45957d78c9
Shivam Gravity <shivamgravity@gmail.com>
2026-03-24 02:24:26 +0530

    removed the main file demo command

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Generate historical data if not present, then start server
 python generate_history.py
-python main.py --demo
 uvicorn api.server:app --host 0.0.0.0 --port ${PORT:-8000}
